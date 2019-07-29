485bcb14bd1bd7c158ba5c2afc226972904447ea
aaronshivers <aaron@moonmail.org>
2019-07-29 16:57:27 -0500

    added datatypes.js

---
+++ homework/dataTypes/dataTypes.js
@@ -0,0 +1,25 @@
+// JavaScript Data Types
+
+// Boolean
+const boolean1 = true
+const boolean2 = false
+
+// Null
+const nulVar = null
+
+// Undefined
+const undefined1 = undefined
+let undefined2
+
+// Number
+const num = 1
+
+// String
+const string = 'string'
+
+// Symbol
+const symbol = Symbol()
+const symbol = Symbol(234)
+
+// BigInt
+const bigInt = 21039841279817234908127340912734091827340978
